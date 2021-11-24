dd943ecf45d7d9b30f982ea4296f8e96e9b8af37
David Soegaard <david.soegaard@gmail.com>
2021-11-24 23:01:01 +0100

    hamburger menu viseble

--- main.py
+++ main.py
@@ -169,11 +169,11 @@ st.dataframe(df)
 
 
 # ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             </style>
+#             """
+#st.markdown(hide_st_style, unsafe_allow_html=True)
